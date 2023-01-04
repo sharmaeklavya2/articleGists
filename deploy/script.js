3b9f2c8203cc6c3d9467a0610e32fd9abf6b046f
Eklavya Sharma <ekurgn@gmail.com>
2023-01-04 14:42:32 +0530

    deploy

---
+++ deploy/script.js
@@ -0,0 +1,75 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3
+// Copyright (C) 2022 Eklavya Sharma. Licensed under GNU GPLv3.
+'use strict';
+
+var articles = null;
+
+function filterArticles(condition) {
+    if(articles !== null) {
+        for(const [id, article] of Object.entries(articles)) {
+            var elem = document.getElementById('article.' + id);
+            if(condition(article)) {
+                elem.classList.remove("hidden");
+            }
+            else {
+                elem.classList.add("hidden");
+            }
+        }
+    }
+}
+
+function hasTopicF(topic) {
+    return function(article) {
+        return article.hasOwnProperty('topics') && article.topics.hasOwnProperty(topic);
+    }
+}
+
+function hasSubtopicF(topic, subtopic) {
+    return function(article) {
+        return hasTopicF(article) && article.topics[topic].includes(subtopic);
+    }
+}
+
+function andF(conditions) {
+    return function(article) {
+        for(const condition of conditions) {
+            if(! condition(article)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+function orF(conditions) {
+    return function(article) {
+        for(const condition of conditions) {
+            if(condition(article)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function installCiteHandlers() {
+    for(var elem of document.getElementsByClassName('citeButton')) {
+        elem.addEventListener('click', function(ev) {
+            var buttonElem = ev.target;
+            const bibtexId = buttonElem.getAttribute('data-target');
+            var bibtexElem = document.getElementById(bibtexId);
+            bibtexElem.classList.toggle('hidden');
+        });
+    }
+}
+
+function main() {
+    window.fetch('articles.json')
+        .then(function(response) {return response.json();})
+        .then(function(data) {articles = data;});
+    installCiteHandlers();
+}
+
+window.addEventListener('load', main);
+
+// @license-end
